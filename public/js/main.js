b1945fbd81d5a724a51cdec18c3606d6df219155
JohannesFeige <johannes.feige@acando.de>
2019-04-10 15:40:08 +0200

    Enhanced animation of mario;

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,35 @@
 import Camera from './Camera.js';
 import Timer from './Timer.js';
 import { loadLevel } from './loaders/level.js';
-import { createMario } from './entities.js';
+import { loadMario } from './entities/Mario.js';
 import { setupKeyboard } from './input.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-Promise.all([createMario(), loadLevel('1-1')]).then(([mario, level]) => {
+Promise.all([loadMario(), loadLevel('1-1')]).then(([createMario, level]) => {
     const camera = new Camera();
     window.camera = camera;
 
+    const mario = createMario();
     mario.pos.set(64, 50);
+
+    mario.addTrait({
+        NAME: 'hacktrait',
+        spawnTimeout: 0,
+        obstruct() {},
+        update(mario, deltaTime) {
+            if (this.spawnTimeout > 0.1 && mario.vel.y < 0) {
+                const spawn = createMario();
+                spawn.pos.x = mario.pos.x;
+                spawn.pos.y = mario.pos.y;
+                spawn.vel.y = mario.vel.y - 200;
+                level.entities.add(spawn);
+                this.spawnTimeout = 0;
+            }
+            this.spawnTimeout += deltaTime;
+        }
+    });
 
     level.entities.add(mario);
 
